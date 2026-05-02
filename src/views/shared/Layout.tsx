e01cfe57e4c77b247ed171a28124e1f6fe232ad1
Isaac Poole <isaacpoolemusic@gmail.com>
2026-05-02 10:25:49 -0400

    Add basic HTML head and meta tags

--- src/views/shared/Layout.tsx
+++ src/views/shared/Layout.tsx
@@ -2,7 +2,14 @@ import type { FC } from "hono/jsx";
 
 export const Layout: FC = ({ children }) => {
   return (
-    <html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="JS-MVC" />
+        <title>JS-MVC</title>
+        {/*<link rel="stylesheet" href="/styles.css" />*/}
+      </head>
       <body>{children}</body>
     </html>
   );
