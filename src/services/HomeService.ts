f5a5b9ea1d4d84c2dc2d1171adefdd6cef53f9b1
Isaac Poole <isaacpoolemusic@gmail.com>
2026-05-04 20:44:53 -0400

    Remove error throw from HomeService.index

--- src/services/HomeService.ts
+++ src/services/HomeService.ts
@@ -1,9 +1,5 @@
-import { NotFoundError } from "../errors";
-
 export class HomeService {
   index() {
-    throw new NotFoundError("nope, not here");
-
     return {
       today: new Date(),
     };
